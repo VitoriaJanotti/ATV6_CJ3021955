702eef7df8b10fc43b7d80c6a854f9b184afe704
Maria Vitória Janotti <160731952+VitoriaJanotti@users.noreply.github.com>
2024-04-10 08:49:31 -0300

    Update Program.cs

--- ATV6_CJ3021955/Program.cs
+++ ATV6_CJ3021955/Program.cs
@@ -68,22 +68,103 @@
 
                 case 4:
                     Console.WriteLine ("EXERCÍCIO 4");
-
+                    
+                    int n1, a1=1;
+                    int soma = 200;
+ 
+                    Console.WriteLine("Digite um número");
+                    n1 = int.Parse(Console.ReadLine())
+                    
+                    while (a1 < soma)
+                    {
+                        if (n1 < 0)
+                        {
+                            Console.WriteLine("número inválido");
+                        }
+                        else
+                        {
+                            Console.WriteLine(a1);
+                        }
+                        a1++;
+                    }
+                    
                     break;
 
                 case 5:
                     Console.Write.Line ("EXERCÍCIO 5");
+
+                      int v, p1=0;
+ 
+                    Console.WriteLine("Digite um número:");
+                    v = int.Parse(Console.ReadLine());
+ 
+                    
+                    while (p1 <= v)
+                    {
+                        if (v % p1 == 0)
+                        {
+                            Console.WriteLine(p1);
+                        }
+                        p1++;
+                    }
 
                     break;
 
                 case 6:
                     Console.Write.Line ("EXERCÍCIO 6");
 
+                    int j1 = 0, v2;
+ 
+                    while (j1 < 11)
+                    {
+                        Console.WriteLine("Digite um valor");
+                        v2 = int.Parse(Console.ReadLine());
+ 
+                        if (v2 < v2)
+                        {
+                            Console.WriteLine("o número maior é = {0}", v2);
+                        }
+ 
+                        if (v2 > v2)
+                        {
+                            Console.WriteLine("o número menor é = {0}", v2);
+                        }
+ 
+                        j1++;
+                    }
+                    
                     break;
 
                 case 7: 
                     Console.Write.Line ("EXERCÍCIO 7")
 
+                        int t = 0, l3;
+                    int r1 = 0, p3 = 0;
+ 
+                    while (t < 10)
+                    {
+                        Console.WriteLine("Digite um valor");
+                        l3 = int.Parse(Console.ReadLine());
+ 
+                        if (l3 <= 0)
+                            break;
+ 
+                        if (t % 2 != 0)
+                        {
+                            r1 = r1 + t;
+                        }
+ 
+                        if (t % 2 == 0)
+                        {
+                            p3 = p3 + t;
+                        }
+ 
+                        Console.WriteLine("soma dos números ímpares = {0}", r1);
+                        Console.WriteLine("soma dos números pares = {0}", p3);
+ 
+                        t++;
+                    }
+                    
                     break;
                     
                 default:
